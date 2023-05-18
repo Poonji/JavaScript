211f281bc3cede951a4dd7f29337d57a0686f365
Poonji <poonjij21@gmail.com>
2023-05-18 16:00:01 +0530

    facy

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-//document.getElementById("count-el").innerText=5
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+let count=0 
 function increment(){
-    console.log("the button was clicked")
+    count=count+1
+    countEl.innerText=count
 }
